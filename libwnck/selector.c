32ca16f6a43d39c5db59b3ce2785087e04377bd6
Vincent Noel <vnoel@cox.net>
2005-01-27 16:57:29 +0000

    Don't crash when a minimized window icon has no alpha. Fixes #165251.
    2005-01-27  Vincent Noel  <vnoel@cox.net>
    
    	* libwnck/selector.c: (wnck_selector_dimm_icon),
    	(wnck_selector_set_window_icon): Don't crash when a minimized
    	window icon has no alpha. Fixes #165251.

--- libwnck/selector.c
+++ libwnck/selector.c
@@ -108,22 +108,26 @@ wnck_selector_get_default_window_icon (void)
   return retval;
 }
 
-static void
+static GdkPixbuf *
 wnck_selector_dimm_icon (GdkPixbuf *pixbuf)
 {
   int x, y, pixel_stride, row_stride;
   guchar *row, *pixels;
   int w, h;
+  GdkPixbuf *dimmed;
 
   w = gdk_pixbuf_get_width (pixbuf);
   h = gdk_pixbuf_get_height (pixbuf);
 
-  g_assert (gdk_pixbuf_get_has_alpha (pixbuf));
+  if (gdk_pixbuf_get_has_alpha (pixbuf)) 
+    dimmed = gdk_pixbuf_copy (pixbuf);
+  else
+    dimmed = gdk_pixbuf_add_alpha (pixbuf, FALSE, 0, 0, 0);
 
   pixel_stride = 4;
 
-  row = gdk_pixbuf_get_pixels (pixbuf);
-  row_stride = gdk_pixbuf_get_rowstride (pixbuf);
+  row = gdk_pixbuf_get_pixels (dimmed);
+  row_stride = gdk_pixbuf_get_rowstride (dimmed);
 
   for (y = 0; y < h; y++)
     {
@@ -135,6 +139,8 @@ wnck_selector_dimm_icon (GdkPixbuf *pixbuf)
         }
       row += row_stride;
     }
+
+  return dimmed;
 }
 
 static void
@@ -142,13 +148,14 @@ wnck_selector_set_window_icon (WnckSelector *selector,
                                GtkWidget *image,
                                WnckWindow *window, gboolean use_icon_size)
 {
-  GdkPixbuf *pixbuf, *freeme;
+  GdkPixbuf *pixbuf, *freeme, *freeme2;
   int width, height;
   int icon_size = -1;
   WnckSelectorPrivate *priv = WNCK_SELECTOR_GET_PRIVATE (selector);
 
   pixbuf = NULL;
   freeme = NULL;
+  freeme2 = NULL;
 
   if (window)
     pixbuf = wnck_window_get_icon (window);
@@ -177,12 +184,17 @@ wnck_selector_set_window_icon (WnckSelector *selector,
     }
 
   if (window && wnck_window_is_minimized (window))
-    wnck_selector_dimm_icon (pixbuf);
+    {      
+      pixbuf = wnck_selector_dimm_icon (pixbuf);
+      freeme2 = pixbuf;
+    }
 
   gtk_image_set_from_pixbuf (GTK_IMAGE (image), pixbuf);
 
   if (freeme)
     g_object_unref (freeme);
+  if (freeme2)
+    g_object_unref (freeme2);
 }
 
 static void
