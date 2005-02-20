490b59642f4ffde96a8d8b06339d88c8b85780d9
Elijah Newren <newren@gmail.com>
2005-02-20 07:28:57 +0000

    Backport of fix in the development branch; patch from Juerg Billeter to
    2005-02-20  Elijah Newren  <newren@gmail.com>
    
    	Backport of fix in the development branch; patch from Juerg
    	Billeter to fix windows from all screen being shown when tasklist
    	is first created.
    
    	* libwnck/tasklist.c: (wnck_tasklist_realize):
            update lists after realization.

--- libwnck/tasklist.c
+++ libwnck/tasklist.c
@@ -1198,6 +1198,8 @@ wnck_tasklist_realize (GtkWidget *widget)
 #endif
   
   (* GTK_WIDGET_CLASS (tasklist_parent_class)->realize) (widget);
+
+  wnck_tasklist_update_lists (tasklist);
 }
 
 static void
