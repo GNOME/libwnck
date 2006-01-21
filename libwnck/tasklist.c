798c5ff5f003d3bd4d28464b5bcba64ea2c5be2b
Elijah Newren <newren gmail com>
2006-01-21 19:09:50 +0000

    include demands-attention/urgent windows unconditionally in tasklist; this
    2006-01-21  Elijah Newren  <newren gmail com>
    
    	* libwnck/tasklist.c (tasklist_include_window_ignoring_skip_taskbar):
    	include demands-attention/urgent windows unconditionally in
    	tasklist; this should help users not miss important windows on
    	other workspaces.  #305979

--- libwnck/tasklist.c
+++ libwnck/tasklist.c
@@ -1865,7 +1865,8 @@ tasklist_include_window_ignoring_skip_taskbar (WnckTasklist *tasklist,
   if (active_workspace == NULL)
     return TRUE;
 
-  if (active_workspace != wnck_window_get_workspace (win))
+  if (active_workspace != wnck_window_get_workspace (win) &&
+      !wnck_window_or_transient_needs_attention (win))
     return FALSE;
 
   if (!wnck_workspace_is_virtual (active_workspace))
