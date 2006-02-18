1b03a9e9e4ce4ee685bc0d8a298ade5664aca801
Vincent Untz <vuntz@gnome.org>
2006-02-18 15:47:57 +0000

    really fix the bug :-)
    2006-02-18  Vincent Untz  <vuntz@gnome.org>
    
    	* libwnck/pager.c: (wnck_pager_button_release): really fix the bug :-)

--- libwnck/pager.c
+++ libwnck/pager.c
@@ -1261,12 +1261,13 @@ wnck_pager_button_release (GtkWidget        *widget,
 
 	  if (space)
             {
+              if (wnck_window_get_workspace (pager->priv->drag_window) != space)
+                pager->priv->action_window = pager->priv->drag_window;
+
               wnck_window_move_to_workspace (pager->priv->drag_window,
                                              space);
               if (space == wnck_screen_get_active_workspace (pager->priv->screen))
                 wnck_window_activate (pager->priv->drag_window, event->time);
-              else
-                pager->priv->action_window = pager->priv->drag_window;
             }
 	}
       
