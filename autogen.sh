ce2231175f2bfea6ddaefb4fff8a2fe37a8d6613
Vincent Untz <vuntz@gnome.org>
2009-04-24 03:25:03 +0200

    Make autogen.sh more modern.

--- autogen.sh
+++ autogen.sh
@@ -1,34 +1,22 @@
-#! /bin/sh
+#!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
 PKG_NAME="libwnck"
+REQUIRED_AUTOMAKE_VERSION=1.9
 
 (test -f $srcdir/configure.in \
-  && test -d $srcdir/libwnck \
   && test -f $srcdir/libwnck/libwnck.h) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
-    echo " top-level libwnck directory"
+    echo " top-level $PKG_NAME directory"
     exit 1
 }
 
-ifs_save="$IFS"; IFS=":"
-for dir in $PATH ; do
-  test -z "$dir" && dir=.
-  if test -f $dir/gnome-autogen.sh ; then
-    gnome_autogen="$dir/gnome-autogen.sh"
-    gnome_datadir=`echo $dir | sed -e 's,/bin$,/share,'`
-    break
-  fi
-done
-IFS="$ifs_save"
+which gnome-autogen.sh || {
+    echo "You need to install gnome-common."
+    exit 1
+}
 
-if test -z "$gnome_autogen" ; then
-  echo "You need to install the gnome-common module and make"
-  echo "sure the gnome-autogen.sh script is in your \$PATH."
-  exit 1
-fi
-
-REQUIRED_AUTOMAKE_VERSION=1.6 GNOME_DATADIR="$gnome_datadir" USE_GNOME2_MACROS=1 . $gnome_autogen
+. gnome-autogen.sh
