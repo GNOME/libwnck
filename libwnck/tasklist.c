d2e1a77e9a92a95c0a79d0aeaaaa49bafc02d3cc
Alberts Muktupāvels <alberts.muktupavels@gmail.com>
2016-03-22 17:28:18 +0200

    tasklist: avoid deprecated GdkDeviceManager

--- libwnck/tasklist.c
+++ libwnck/tasklist.c
@@ -2628,7 +2628,7 @@ wnck_task_position_menu (GtkMenu   *menu,
 {
   GtkWidget *widget = GTK_WIDGET (user_data);
   GdkWindow *window;
-  GdkDeviceManager *dev_manager;
+  GdkSeat *seat;
   GdkDevice *pointer;
   GtkAllocation allocation;
   GtkRequisition requisition;
@@ -2652,8 +2652,8 @@ wnck_task_position_menu (GtkMenu   *menu,
   else
     menu_ypos += allocation.height;
 
-  dev_manager = gdk_display_get_device_manager (gtk_widget_get_display (widget));
-  pointer = gdk_device_manager_get_client_pointer (dev_manager);
+  seat = gdk_display_get_default_seat (gtk_widget_get_display (widget));
+  pointer = gdk_seat_get_pointer (seat);
   gdk_window_get_device_position (window, pointer, &pointer_x, &pointer_y, NULL);
 
   if (requisition.width < pointer_x)
