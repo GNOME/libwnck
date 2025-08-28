693f1ac6a4653c4f1247672bcb088a19fd4461ec
Alberts Muktupāvels <alberts.muktupavels@gmail.com>
2025-08-28 20:29:51 +0300

    window: use correct variable name
    Commit b416f9b3465e was supposed to set has_group_leader variable!
    
    https://gitlab.gnome.org/GNOME/libwnck/-/issues/166

--- libwnck/window.c
+++ libwnck/window.c
@@ -3133,7 +3133,7 @@ update_wmhints (WnckWindow *window)
 
       if (hints->flags & WindowGroupHint)
         {
-          window->priv->group_leader = TRUE;
+          window->priv->has_group_leader = TRUE;
           window->priv->group_leader = hints->window_group;
         }
 
